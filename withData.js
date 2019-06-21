2f8606db2587661ab5125ca7345442bb2d1e4a24
drobinson001 <34065328+drobinson001@users.noreply.github.com>
2019-06-21 13:42:35 +0100

    change config setup

--- withData.js
+++ withData.js
@@ -1,12 +1,4 @@
 import { withAppSyncData } from 'next-apollo-appsync';
-
-const config = {
-  url: "https://onq4ydh7anf7teujf56cv6b3b4.appsync-api.us-east-2.amazonaws.com/graphql",
-  region: "us-east-2",
-  auth: {
-    type: "API_KEY",
-    apiKey: "da2-cowazpzhendcfhzrvazz3nfqxy"
-  },
-}
+import config from './appsync-config';
 
 export default withAppSyncData(config)
