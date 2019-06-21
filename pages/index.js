fc019b742ab69ae36241d3e64c801f34a51c4243
drobinson001 <34065328+drobinson001@users.noreply.github.com>
2019-06-21 11:39:40 +0100

    add initial changes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-import gql from 'graphql-tag'
-import { graphql, compose } from 'react-apollo'
-import withData from '../withData'
-import uuidV4 from 'uuid/v4'
+import gql from "graphql-tag";
+import { graphql, compose } from "react-apollo";
+import withData from "../withData";
+import uuidV4 from "uuid/v4";
+import Link from 'next/link';
+
+const PostLink = props => (
+  <li>
+    <Link as={`/p/${props.title}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 
 const query = gql`
   query listTodos {
@@ -13,57 +22,61 @@ const query = gql`
       }
     }
   }
-`
+`;
 
 const mutation = gql`
   mutation createTodo($id: ID!, $name: String!, $completed: Boolean!) {
-    createTodo(input: {
-      id: $id,
-      name: $name,
-      completed: $completed
-    }) {
+    createTodo(input: { id: $id, name: $name, completed: $completed }) {
       id
       name
       completed
     }
   }
-`
+`;
 
 const subscription = gql`
   subscription onCreateTodo {
     onCreateTodo {
       id
       name
-      completed 
+      completed
     }
   }
-`
+`;
 
 class App extends React.Component {
-  state = { todo: '' }
+  state = { todo: "" };
   componentDidMount() {
-    this.props.subscribeToNewTodos()
+    this.props.subscribeToNewTodos();
   }
   createTodo = () => {
     const todo = {
       name: this.state.todo,
       id: uuidV4(),
       completed: false
-    }
-    this.props.createTodo(todo)
-    this.setState({ todo: '' })
-  }
+    };
+    this.props.createTodo(todo);
+    this.setState({ todo: "" });
+  };
   render() {
-    return <div>
-      <p>Hello World</p>
-      <input value={this.state.todo} onChange={e => this.setState({ todo: e.target.value })} />
-      <button onClick={this.createTodo}>Create Todo</button>
-      {
-        this.props.todos.map((todo, index) => (
+    return (
+      <div>
+        <p>Hello World</p>
+        <input
+          value={this.state.todo}
+          onChange={e => this.setState({ todo: e.target.value })}
+        />
+        <button onClick={this.createTodo}>Create Todo</button>
+        {this.props.todos.map((todo, index) => (
           <p key={index}>{todo.name}</p>
-        ))
-      }
-    </div>
+        ))}
+        <ul>
+        <PostLink title="Hello Next.js" name="hello" />
+        <PostLink title="Learn Next.js is awesome" name="learn" />
+        <PostLink title="Deploy apps with Zeit" name="deploy" />
+      </ul>
+      </div>
+    );
   }
 }
 
@@ -74,41 +87,53 @@ const AppWithTodos = compose(
         props.mutate({
           variables: todo,
           optimisticResponse: {
-            __typename: 'Mutation',
-            createTodo: { ...todo,  __typename: 'Todo' }
+            __typename: "Mutation",
+            createTodo: { ...todo, __typename: "Todo" }
           },
           update: (proxy, { data: { createTodo } }) => {
             const data = proxy.readQuery({ query: query });
             data.listTodos.items.unshift(createTodo);
             proxy.writeQuery({ query: query, data });
           }
-        })
+        });
       }
     })
   }),
   graphql(query, {
     options: {
-      fetchPolicy: 'cache-and-network'
+      fetchPolicy: "cache-and-network"
     },
     props: props => ({
       todos: props.data.listTodos ? props.data.listTodos.items : [],
       subscribeToNewTodos: params => {
         props.data.subscribeToMore({
           document: subscription,
-          updateQuery: (prev, { subscriptionData: { data : { onCreateTodo } } }) => {
-            console.log('onCreateTodo: ', onCreateTodo)
+          updateQuery: (
+            prev,
+            {
+              subscriptionData: {
+                data: { onCreateTodo }
+              }
+            }
+          ) => {
+            console.log("onCreateTodo: ", onCreateTodo);
             return {
               ...prev,
               listTodos: {
-                __typename: 'TodoConnection',
-                items: [onCreateTodo, ...prev.listTodos.items.filter(todo => todo.id !== onCreateTodo.id)]
+                __typename: "TodoConnection",
+                items: [
+                  onCreateTodo,
+                  ...prev.listTodos.items.filter(
+                    todo => todo.id !== onCreateTodo.id
+                  )
+                ]
               }
-            }
+            };
           }
-        })
+        });
       }
     })
   })
-)(App)
+)(App);
 
-export default withData(AppWithTodos)
+export default withData(AppWithTodos);
--- withData.js
+++ withData.js
@@ -1,4 +1,12 @@
-import { withAppSyncData } from 'next-apollo-appsync'
-import AppSyncConfig from './AppSync'
+import { withAppSyncData } from 'next-apollo-appsync';
 
-export default withAppSyncData(AppSyncConfig)
+const config = {
+  url: "https://onq4ydh7anf7teujf56cv6b3b4.appsync-api.us-east-2.amazonaws.com/graphql",
+  region: "us-east-2",
+  auth: {
+    type: "API_KEY",
+    apiKey: "da2-cowazpzhendcfhzrvazz3nfqxy"
+  },
+}
+
+export default withAppSyncData(config)
