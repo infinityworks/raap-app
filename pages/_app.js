207503270647dc9ca198bf14ee71d02dfe236f90
drobinson001 <34065328+drobinson001@users.noreply.github.com>
2019-06-21 15:38:02 +0100

    SSR Styles

---
+++ pages/_app.js
@@ -0,0 +1,42 @@
+import App, { Container } from "next/app";
+import React, { Fragment } from "react";
+// import { ApolloProvider } from "react-apollo";
+
+// import withApolloClient from "../lib/with-apollo-client";
+import { ThemeProvider } from "styled-components";
+import ResetStyle from "../components/ResetStyle";
+
+const theme = {
+  colors: {
+    primary: "#0070f3"
+  }
+};
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <ThemeProvider theme={theme}>
+          <Fragment>
+            <ResetStyle />
+            <h1>TEST</h1>
+            <Component {...pageProps} />
+          </Fragment>
+        </ThemeProvider>
+      </Container>
+    );
+  }
+}
+
+export default (MyApp);
--- withData.js
+++ withData.js
@@ -1,4 +1,11 @@
-import { withAppSyncData } from 'next-apollo-appsync';
-import config from './appsync-config';
+import { withAppSyncData } from "next-apollo-appsync";
+import config from "./aws-exports";
 
-export default withAppSyncData(config)
+export default withAppSyncData({
+  url: config.graphqlEndpoint,
+  region: config.region,
+  auth: {
+    type: config.authenticationType,
+    key: config.apiKey
+  }
+});
