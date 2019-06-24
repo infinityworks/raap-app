de00a1e83fbea5ad24245bc3eebb54d91a0e344b
drobinson001 <34065328+drobinson001@users.noreply.github.com>
2019-06-24 13:45:52 +0100

    setup serverless

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-const express = require('express');
-const next = require('next');
+const express = require("express");
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production';
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app
-  .prepare()
-  .then(() => {
-    const server = express();
+const createServer = () => {
+  const server = express();
+  server.get("*", (req, res) => handle(req, res));
+  return server;
+};
 
-    server.get('/p/:title', (req, res) => {
-      const actualPage = '/post';
-      const queryParams = { title: req.params.title };
-      app.render(req, res, actualPage, queryParams);
-    });
+const server = createServer();
 
-    server.get('*', (req, res) => {
-      // console.log(req)
-      return handle(req, res);
+if (!process.env.LAMBDA) {
+  app
+    .prepare()
+    .then(() => {
+      server.listen(port, err => {
+        if (err) throw err;
+        console.log(`Ready on http://localhost:${port}`);
+      });
+    })
+    .catch(ex => {
+      console.error(ex.stack);
+      process.exit(1);
     });
+}
 
-    server.listen(3000, err => {
-      if (err) throw err;
-      console.log('> Ready on http://localhost:3000');
-    });
-  })
-  .catch(ex => {
-    console.error(ex.stack);
-    process.exit(1);
-  });
+export const app;
+export const server;
