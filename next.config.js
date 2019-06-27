e4f68654251693b01e2e4fdd960b758aed34f614
drobinson001 <34065328+drobinson001@users.noreply.github.com>
2019-06-27 10:51:51 +0100

    improve build configuration and scripts

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 module.exports = {
+  // assetPrefix: 'https://s3.amazonaws.com/infinity-works-daniel-r-assets',
   target: 'serverless',
   // useFileSystemPublicRoutes: false,
-  webpack: (config) => {
+  webpack: config => {
     // Fixes npm packages that depend on `fs` module
+    // eslint-disable-next-line no-param-reassign
     config.node = {
       fs: 'empty',
     };
 
     return config;
   },
-}
-;
+};
--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,6 @@ const routes = require('next-routes');
 
 // NOTE: Changes to routing require a rebuild.
 module.exports = routes()
-  .add({ name: 'home', pattern: '/', page: 'home' })
+  .add({ name: 'home', pattern: '/', page: 'index' })
   .add({ name: 'todos' })
   .add({ name: 'post', pattern: '/post/:title', page: 'post' });
